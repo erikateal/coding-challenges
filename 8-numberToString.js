cf3023393ccf374da734cb20f2fb14cf2b808c7b
Erika Teal <erikamw2070@yahoo.com>
2022-03-28 21:50:44 -0500

    3/28 coding challenge

---
+++ 8-numberToString.js
@@ -0,0 +1,20 @@
+//OBJECTIVE
+// Convert a Number to a String
+
+//LINK
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+// INSTRUCTIONS
+// We need a function that can transform a number into a string.
+
+// MY SOLUTION
+function numberToString(num) {
+    return num.toString();
+  }
+
+// BEST PRACTICE
+//same as above
+
+// WHAT I LEARNED
+// .toString() method
+// The toString() method returns a string representing the specified Number object.
