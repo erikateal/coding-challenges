cf0029d14426cd9c9c97a294105ca2b4c150c459
Erika Teal <erikamw2070@yahoo.com>
2022-03-30 11:59:05 -0500

    3/30 coding challenge

---
+++ 8-evenOfOdd.js
@@ -0,0 +1,25 @@
+//OBJECTIVE
+// Even of Odd
+
+//LINK
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+// INSTRUCTIONS
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// MY SOLUTION
+function even_or_odd(number) {
+    if (number % 2 === 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
+  
+  // BEST PRACTICE
+  // function even_or_odd(number) {
+  //   return number % 2 ? "Odd" : "Even"
+  // }
+  
+  // WHAT I LEARNED
+  // The boolean value of 0 is false or falsy, so setting 'number % 2 === 0' is not necessary. 
