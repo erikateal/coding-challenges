569c042bf4ceb4529edc7259ac1381e5835828d9
Erika Teal <erikamw2070@yahoo.com>
2022-04-14 13:30:41 -0500

    4/9 coding challenge

---
+++ 8-countSheep.js
@@ -0,0 +1,28 @@
+//OBJECTIVE
+// If you can't sleep, just count sheep!!
+
+//LINK
+// https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+
+// INSTRUCTIONS
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+// MY SOLUTION
+var countSheep = function (num){
+    let result = ''
+    for (let i=1; i<=num; i++) {
+      result += `${i} sheep...`; 
+    }
+    return result;
+  }
+  
+  // BEST PRACTICE
+  // var countSheep = function (num){
+  //   let str = "";
+  //   for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+  //   return str;
+  // }
+  
+  // WHAT I LEARNED
+  //
