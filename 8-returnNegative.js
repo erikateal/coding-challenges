7fa100214950cd55c5506070d3b68a21a49f5ad4
Erika Teal <erikamw2070@yahoo.com>
2022-04-14 13:53:12 -0500

    3/28 coding challenge

--- 8-returnNegative.js
+++ 8-returnNegative.js
@@ -9,7 +9,7 @@
 // The The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
-// MY SOLUTION
+// MY SOLUTION 
 function makeNegative(num) {
     if (num > 0) {
       return -num
