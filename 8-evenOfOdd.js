69ff639fd925f4756effcbb54a03643dc9252bd9
Erika Teal <erikamw2070@yahoo.com>
2022-04-14 13:51:56 -0500

    3/30 coding challenge

--- 8-evenOfOdd.js
+++ 8-evenOfOdd.js
@@ -4,7 +4,7 @@
 //LINK
 // https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
 
-// INSTRUCTIONS
+// INSTRUCTIONS 
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 // MY SOLUTION
