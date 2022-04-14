01a70d3ecf6767da3b06bdfb4bed31fcd2075df7
Erika Teal <erikamw2070@yahoo.com>
2022-04-14 14:02:50 -0500

    3/17 coding challenge

---
+++ 8-makeUpperCase.js
@@ -0,0 +1,19 @@
+//OBJECTIVE
+// MakeUpperCase
+
+//LINK
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+// INSTRUCTIONS
+// Write a function which converts the input string to uppercase.
+
+// MY SOLUTION
+function makeUpperCase(str) {
+    return str.toUpperCase();
+  }
+  
+  // BEST PRACTICE
+  // const makeUpperCase = str => str.toUpperCase();      
+  
+  // WHAT I LEARNED
+  // .toUpperCase() method returns the calling string value converted to uppercase (the value will be converted to a string if it isn't one).
