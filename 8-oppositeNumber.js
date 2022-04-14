7bbca782badfc6ccbfd64151ddde19df0543c770
Erika Teal <erikamw2070@yahoo.com>
2022-04-14 14:14:06 -0500

    3/23 coding challenge

---
+++ 8-oppositeNumber.js
@@ -0,0 +1,24 @@
+//OBJECTIVE
+// Opposite number
+
+//LINK
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+// INSTRUCTIONS
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+// MY SOLUTION
+function opposite(number) {
+    return -number
+  }
+  
+  // BEST PRACTICE
+  // same as above
+  
+  // WHAT I LEARNED
+  //
