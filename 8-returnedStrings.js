6acd19f552574423ee27d2dbf9c3b47be216ef4e
Erika Teal <erikamw2070@yahoo.com>
2022-04-14 13:43:10 -0500

    4/12 coding challenge

---
+++ 8-returnedStrings.js
@@ -0,0 +1,21 @@
+//OBJECTIVE
+// Returning Strings
+
+//LINK
+// https://www.codewars.com/kata/55a70521798b14d4750000a4
+
+// INSTRUCTIONS
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// MY SOLUTION
+function greet(name){
+  return `Hello, ${name} how are you doing today?`;
+}
+
+// BEST PRACTICE
+// same as above
+
+// WHAT I LEARNED
+// concatentation and template literals
