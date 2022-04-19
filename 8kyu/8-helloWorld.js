62a3c29be553979f216c4fe7e944c05c57de37ed
Erika Teal <erikamw2070@yahoo.com>
2022-04-19 13:21:27 -0500

    4/19 coding challenge

---
+++ 8kyu/8-helloWorld.js
@@ -0,0 +1,23 @@
+//OBJECTIVE
+// Function 1 - hello world
+
+//LINK
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+// INSTRUCTIONS
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+// MY SOLUTION
+let greet = () => 'hello world!'
+
+// BEST PRACTICE
+// function greet() {
+//     return "hello world!";
+// }
+
+// WHAT I LEARNED
+// Practiced arrow functions
