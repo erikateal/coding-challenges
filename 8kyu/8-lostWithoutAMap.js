adae328cd50d03e1f8a2000f1c3e233bb3217f7b
Erika Teal <erikamw2070@yahoo.com>
2022-04-20 22:04:28 -0500

    4/21 coding challenge

---
+++ 8kyu/8-lostWithoutAMap.js
@@ -0,0 +1,22 @@
+//OBJECTIVE
+// Beginner - Lost Without a Map
+
+//LINK
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+
+// INSTRUCTIONS
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// MY SOLUTION
+function maps(x){
+    return x.map(x => x*2)
+  }
+
+// BEST PRACTICE
+// function maps(x){
+//   return x.map(n => n * 2);
+// }
