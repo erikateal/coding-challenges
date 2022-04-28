3c9b6c7db38227782f6c6834fe424ee81221bc58
Erika Teal <erikamw2070@yahoo.com>
2022-04-28 16:32:21 -0500

    4/27 coding challenge

--- 8kyu/8-invertValues.js
+++ 8kyu/8-invertValues.js
@@ -2,7 +2,7 @@
 // Invert values
 
 //LINK
-// 
+// https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
 
 // INSTRUCTIONS
 // Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
