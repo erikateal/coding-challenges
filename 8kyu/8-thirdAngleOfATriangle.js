358a6310cc9a7e88252dcf3d3a3897ffc30a15c1
Erika Teal <erikamw2070@yahoo.com>
2022-05-03 14:47:34 -0500

    4/28 coding challenge

---
+++ 8kyu/8-thirdAngleOfATriangle.js
@@ -0,0 +1,20 @@
+//OBJECTIVE
+// Third Angle of a Triangle
+
+//LINK
+// https://www.codewars.com/kata/5a023c426975981341000014
+
+// INSTRUCTIONS
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// MY SOLUTION
+function otherAngle(a, b) {
+    return 180 - a - b;
+  }
+
+// BEST PRACTICE
+// same as above
