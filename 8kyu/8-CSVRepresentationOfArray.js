bc0f2852cb7d51f93a8811aa76ce8afb5de1ded6
Erika Teal <erikamw2070@yahoo.com>
2022-05-06 14:59:12 -0500

    5/4 coding challenge

---
+++ 8kyu/8-CSVRepresentationOfArray.js
@@ -0,0 +1,35 @@
+//OBJECTIVE
+// CSV representation of array
+
+//LINK
+// https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036
+
+// INSTRUCTIONS
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+// Example:
+
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]] 
+    
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+// Array's length > 2.
+
+// MY SOLUTION
+function toCsvText(array) {
+    for(let i=0; i<array.length; i++)
+      array[i]=array[i].join(',')
+    return array.join('\n')
+  }
+
+// BEST PRACTICE
+// function toCsvText(array) {
+//     return array.join('\n');
+// }
