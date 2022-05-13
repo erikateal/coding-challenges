4e7f25a7323ffaef7bca8bf75e7c1f5fd6b6e41d
Erika Teal <erikamw2070@yahoo.com>
2022-05-12 21:52:52 -0500

    5/13 coding challenge

---
+++ 8kyu/8-findTheRemainder.js
@@ -0,0 +1,37 @@
+//OBJECTIVE
+// Find the Remainder
+
+//LINK
+// https://www.codewars.com/kata/524f5125ad9c12894e00003f
+
+// INSTRUCTIONS
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+
+// Examples:
+// n = 17
+// m = 5
+// result = 2 (remainder of `17 / 5`)
+
+// n = 13
+// m = 72
+// result = 7 (remainder of `72 / 13`)
+
+// n = 0
+// m = -1
+// result = 0 (remainder of `0 / -1`)
+
+// n = 0
+// m = 1
+// result - division by zero (refer to the specifications on how to handle this in your language)
+
+// MY SOLUTION
+function remainder(n, m){
+    return Math.max(n,m) % Math.min(n,m)
+  }
+
+// BEST PRACTICE
+// function remainder(a, b){
+//   return a > b ? a % b : b % a;
+// }
