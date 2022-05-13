8382dabd97cdd30b0eda6232e2a48eee0b816d93
Erika Teal <erikamw2070@yahoo.com>
2022-05-12 19:31:29 -0500

    5/13 coding challenge

---
+++ 8kyu/8-theWideMouthedFrog.js
@@ -0,0 +1,16 @@
+//OBJECTIVE
+// The Wide-Mouthed frog!
+
+//LINK
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/
+
+// INSTRUCTIONS
+// 
+
+// MY SOLUTION
+function mouthSize(animal) {
+    return animal.toLowerCase() === 'alligator' ? 'small' : 'wide'
+  }
+
+// BEST PRACTICE
+// same as above
