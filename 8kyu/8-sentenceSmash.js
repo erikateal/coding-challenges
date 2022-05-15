e217481568e459c073cecb3af97f60bfd3fa0639
Erika Teal <erikamw2070@yahoo.com>
2022-05-14 20:07:40 -0500

    5/14 coding challenge

---
+++ 8kyu/8-sentenceSmash.js
@@ -0,0 +1,19 @@
+//OBJECTIVE
+// Sentence Smash
+
+//LINK
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+// INSTRUCTIONS
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+// MY SOLUTION
+function smash (words) {
+    return words.join(' ')
+ };
+
+// BEST PRACTICE
+// same as above
