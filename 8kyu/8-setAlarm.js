72a9a08267fbb46cb9a9a6f257c0f4709af0c2d7
Erika Teal <erikamw2070@yahoo.com>
2022-05-14 20:16:32 -0500

    5/14 coding challenge

---
+++ 8kyu/8-setAlarm.js
@@ -0,0 +1,21 @@
+//OBJECTIVE
+// L1: Set Alarm
+
+//LINK
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038
+
+// INSTRUCTIONS
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+// MY SOLUTION
+const setAlarm = (employed, vacation) => employed === true && vacation === false ? true : false
+
+// BEST PRACTICE
+// const setAlarm = (employed, vacation) => employed && !vacation;
