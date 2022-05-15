e665b48d13f3e188aa01ac4ffa1d555e48110126
Erika Teal <erikamw2070@yahoo.com>
2022-05-14 20:46:46 -0500

    5/14 coding challenge

---
+++ 8kyu/8-parseNiceIntFromCharProblem.js
@@ -0,0 +1,25 @@
+//OBJECTIVE
+// 
+
+//LINK
+// https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1
+
+// INSTRUCTIONS
+// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+// Write a program that returns the girl's age (0-9) as an integer.
+
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+// MY SOLUTION
+function getAge(inputString){
+    return Number(inputString.split('').slice(0, 1).join(''))
+  }
+
+// BEST PRACTICE
+// function getAge(inputString){
+//   return parseInt(inputString);
+// }
+
+// WHAT I LEARNED
+// parseInt() method parses a value as a string and returns the first integer.
