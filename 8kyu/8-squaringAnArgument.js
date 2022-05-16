576d261164a0cf1cd9f653e445d99fc257506b79
Erika Teal <erikamw2070@yahoo.com>
2022-05-15 23:27:13 -0500

    5/15 coding challenge

---
+++ 8kyu/8-squaringAnArgument.js
@@ -0,0 +1,16 @@
+//OBJECTIVE
+// 
+
+//LINK
+// https://www.codewars.com/kata/523b623152af8a30c6000027
+
+// INSTRUCTIONS
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// MY SOLUTION
+const square = (num) => Math.pow(num, 2);
+
+// BEST PRACTICE
+// var square = function(a){
+//   return a*a;
+// }
