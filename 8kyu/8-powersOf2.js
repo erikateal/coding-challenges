970dbe1abf2607644c8ea127074a47a76a5bc886
Erika Teal <erikamw2070@yahoo.com>
2022-05-23 12:08:06 -0500

    5/23 coding challenge

---
+++ 8kyu/8-powersOf2.js
@@ -0,0 +1,24 @@
+//OBJECTIVE
+// Powers of 2
+
+//LINK
+// https://www.codewars.com/kata/57a083a57cb1f31db7000028
+
+// INSTRUCTIONS
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+// MY SOLUTION
+function powersOfTwo(n){
+    let arr = [];
+    for(let i = 0; i <= n; i++)
+      arr.push(Math.pow(2,i));
+    return arr;
+  }
+
+// BEST PRACTICE
+// same as above
