7eab64430a00c87697c9ce19d387dbf494df38fa
Erika Teal <erikamw2070@yahoo.com>
2022-05-25 16:01:49 -0500

    5/25 coding challenge

---
+++ 7kyu/7-sumOfTwoLowestPositiveIntegers.js
@@ -0,0 +1,26 @@
+//OBJECTIVE
+// Sum of two lowest positive integers
+
+//LINK
+// https://www.codewars.com/kata/558fc85d8fd1938afb000014
+
+// INSTRUCTIONS
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+// MY SOLUTION
+function sumTwoSmallestNumbers(numbers) {  
+    if(numbers.length > 4){
+      return numbers.sort((a,b) => b-a).splice(3).reduce((acc,c) => acc+c,0)
+    } 
+      return numbers.sort((a,b) => b-a).splice(2).reduce((acc,c) => acc+c,0)
+  }
+
+// BEST PRACTICE
+// function sumTwoSmallestNumbers(numbers){  
+//   numbers = numbers.sort(function(a, b){return a - b; });
+//   return numbers[0] + numbers[1];
+// };
