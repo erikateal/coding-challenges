5a8c7f87339734884e745c9d836a4048aaed68eb
Erika Teal <erikamw2070@yahoo.com>
2022-05-28 13:32:44 -0500

    edited file names

--- 7kyu/descendingOrder.js
+++ 7kyu/descendingOrder.js
@@ -18,6 +18,7 @@
 function descendingOrder(n){
     return +(n.toString().split('').sort((a,b) => b-a).join(''))
   }
+console.log(descendingOrder(546412313))
 
 // BEST PRACTICE
 // function descendingOrder(n){
