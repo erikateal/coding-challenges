4934b3986baea96f87445f3f63adbbe9b96297b9
Erika Teal <erikamw2070@yahoo.com>
2022-06-03 15:52:16 -0500

    6/3 coding challenge

---
+++ 8kyu/binToDecimal.js
@@ -0,0 +1,16 @@
+//OBJECTIVE
+// Bin to Decimal
+
+//LINK
+// https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
+
+// INSTRUCTIONS
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// MY SOLUTION
+function binToDec(bin){
+    return parseInt(bin,2);
+  }
+
+// BEST PRACTICE
+// same as above
