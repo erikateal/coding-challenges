f03184e7b283ecb09fd0c88075114bff5a32612b
Erika Teal <erikamw2070@yahoo.com>
2022-06-10 17:45:03 -0500

    6/10 coding challenge

---
+++ 8kyu/hexToDecimal.js
@@ -0,0 +1,16 @@
+//OBJECTIVE
+// Hex to Decimal
+
+//LINK
+// https://www.codewars.com/kata/57a4d500e298a7952100035d
+
+// INSTRUCTIONS
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// MY SOLUTION
+function hexToDec(hexString){
+    return  parseInt(hexString, 16);
+   }
+
+// BEST PRACTICE
+// same as above
