7a0de2d1bacb24a9df2713be8a92c1e082fcd069
Erika Teal <erikamw2070@yahoo.com>
2022-06-11 21:34:04 -0500

    6/11 coding challenge

---
+++ 8kyu/functionSyntaxDebugging.js
@@ -0,0 +1,16 @@
+//OBJECTIVE
+// Grasshopper - Function syntax debugging
+
+//LINK
+// https://www.codewars.com/kata/56dae9dc54c0acd29d00109a
+
+// INSTRUCTIONS
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+// MY SOLUTION
+function main (verb, noun) {
+    return verb + noun
+  }
+
+// BEST PRACTICE
+// same as above
