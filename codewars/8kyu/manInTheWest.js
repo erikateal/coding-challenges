c1b841b3b12bc24a63421f3a6811b08cd0424424
Erika Teal <erikamw2070@yahoo.com>
2022-06-22 10:21:50 -0500

    6/22 coding challenge

---
+++ codewars/8kyu/manInTheWest.js
@@ -0,0 +1,18 @@
+//OBJECTIVE
+// Man in the west
+
+//LINK
+// https://www.codewars.com/kata/59bd5dc270a3b7350c00008b
+
+// INSTRUCTIONS
+// A western man is trying to find gold in a river. To do that, he passes a bucket through the river's soil and then checks if it contains any gold. However, he could be more productive if he wrote an algorithm to do the job for him.
+
+// So, you need to check if there is gold in the bucket, and if so, return True/true. If not, return False/false.
+
+// MY SOLUTION
+function checkTheBucket(bucket){
+    return bucket.includes('gold');
+  }
+
+// BEST PRACTICE
+// same as above
