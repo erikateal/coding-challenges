48abfd78d84690d0b8da5febfaa908ed6b593865
Erika Teal <erikamw2070@yahoo.com>
2022-06-26 11:29:13 -0500

    6/26 coding challenge

---
+++ codewars/8kyu/basicTrainingAddItemToAnArray.js
@@ -0,0 +1,17 @@
+//OBJECTIVE
+// Basic Training: Add item to an Array
+
+//LINK
+// https://www.codewars.com/kata/511f0fe64ae8683297000001
+
+// INSTRUCTIONS
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+// MY SOLUTION
+websites.push('codewars')
+
