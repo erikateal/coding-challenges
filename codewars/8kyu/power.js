cbb87b0025820456c77f450c062e37818257d67a
Erika Teal <erikamw2070@yahoo.com>
2022-06-30 15:07:09 -0500

    6/30 coding challenge

---
+++ codewars/8kyu/power.js
@@ -0,0 +1,18 @@
+//OBJECTIVE
+// Power
+
+//LINK
+// https://www.codewars.com/kata/562926c855ca9fdc4800005b
+
+// INSTRUCTIONS
+// 
+
+// MY SOLUTION
+function numberToPower(number, power){
+    let result = 1;
+    for (let i = 0; i < power; i ++) {
+        result *= number 
+    }
+    return result
+}
+
