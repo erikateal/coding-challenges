c24132b5118361d42d03fe39e8f24ddcb7b6c0be
Erika Teal <erikamw2070@yahoo.com>
2022-07-09 22:24:33 -0500

    7/9 coding challenge

---
+++ codewars/8kyu/doesMyListIncludeThis.js
@@ -0,0 +1,17 @@
+//OBJECTIVE
+// Enumerable Magic #3 - Does My List Include This?
+
+//LINK
+// https://www.codewars.com/kata/545991b4cbae2a5fda000158
+
+// INSTRUCTIONS
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+// MY SOLUTION
+function include(arr, item){
+    return arr.includes(item);
+  }
+
+// BEST PRACTICE
+// same as above
+
