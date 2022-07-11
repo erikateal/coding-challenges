6a626b840260e2aecc5f930108cc26cbdbff980e
Erika Teal <erikamw2070@yahoo.com>
2022-07-10 22:32:55 -0500

    7/10 coding challenge

---
+++ codewars/8kyu/5WithoutNumbers.js
@@ -0,0 +1,17 @@
+//OBJECTIVE
+// 5 without numbers !!
+
+//LINK
+// https://www.codewars.com/kata/59441520102eaa25260000bf
+
+// INSTRUCTIONS
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// MY SOLUTION
+function unusualFive() {
+    let arr = ['a','b','c','d','e']
+     return arr.length
+    }
+
