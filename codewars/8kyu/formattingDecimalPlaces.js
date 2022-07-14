5fdb81206fc62af05f151c9d0cdac6e67a247d8f
Erika Teal <erikamw2070@yahoo.com>
2022-07-14 10:41:40 -0500

    7/14 coding challenge

---
+++ codewars/8kyu/formattingDecimalPlaces.js
@@ -0,0 +1,18 @@
+//OBJECTIVE
+// Formatting decimal places #0
+
+//LINK
+// https://www.codewars.com/kata/5641a03210e973055a00000d
+
+// INSTRUCTIONS
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+// Example:    
+// 5.5589 is rounded 5.56   
+// 3.3424 is rounded 3.34
+
+// MY SOLUTION
+function twoDecimalPlaces(n) {
+    return +n.toFixed(2);
+  }
+
