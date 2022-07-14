07f8658009d9ac1c7aa04abb578995706e3da9e4
Erika Teal <erikamw2070@yahoo.com>
2022-07-14 11:01:31 -0500

    7/14 coding challenge

---
+++ codewars/8kyu/sumOfMultiples.js
@@ -0,0 +1,43 @@
+//OBJECTIVE
+// Sum of Multiples
+
+//LINK
+// https://www.codewars.com/kata/57241e0f440cd279b5000829
+
+// INSTRUCTIONS
+// Find the sum of all multiples of n below m
+
+// Keep in Mind
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+// MY SOLUTION
+function sumMul(n,m){
+    let arr = []
+    if(m < 0 || m === 0 || n === m){
+        return 'INVALID'
+      }
+    for(let i = 0; i<m; i++){
+      if(i%n === 0){
+        arr.push(i)
+      }
+    }
+    return arr.reduce((acc,c) => acc+c, 0)
+  }
+
+// BEST PRACTICE
+// function sumMul(n,m){
+//   if (n >= m) return "INVALID";
+
+//   var sum = 0;
+//     for (var i = n; i < m; i+=n) {
+//       sum += i;
+//     }
+//     return sum;
+//   }
+
