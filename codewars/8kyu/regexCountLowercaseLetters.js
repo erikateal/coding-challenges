817500fb363053e3926e588984be4e1bc2976bbc
Erika Teal <erikamw2070@yahoo.com>
2022-07-15 12:04:13 -0500

    7/14 coding challenge

---
+++ codewars/8kyu/regexCountLowercaseLetters.js
@@ -0,0 +1,32 @@
+//OBJECTIVE
+// Regex count lowercase letters
+
+//LINK
+// https://www.codewars.com/kata/56a946cd7bd95ccab2000055
+
+// INSTRUCTIONS
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// lowercaseCount("abc"); ===> 3
+
+// lowercaseCount("abcABC123"); ===> 3
+
+// lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+
+// lowercaseCount(""); ===> 0;
+
+// lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+
+// lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26
+
+// MY SOLUTION
+function lowercaseCount(str){
+    return (str.match(/[a-z]/g) || '').length
+}
+
+// BEST PRACTICE
+// function lowercaseCount(str){
+//     return (str.match(/[a-z]/g) || []).length
+// }
+
