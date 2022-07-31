48926fb9b91285cfc33d55ff2e78fc2dfe2ef896
Erika Teal <erikamw2070@yahoo.com>
2022-07-31 16:49:22 -0500

    7/31 coding challenge

---
+++ codewars/7kyu/bumbsInTheRoad.js
@@ -0,0 +1,27 @@
+//OBJECTIVE
+// Bumps in the Road
+
+//LINK
+// https://www.codewars.com/kata/57ed30dde7728215300005fa
+
+// INSTRUCTIONS
+// Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+// MY SOLUTION
+function bump(x){
+  let count = 0
+  for(let i=0; i<=x.length; i++){
+    if(x[i] === 'n'){
+      count++
+    }
+  }
+  if(count <= 15){
+    return 'Woohoo!'
+  }
+    return 'Car Dead'
+}
+
+// BEST PRACTICE
+// const bump=x=>x.split('n').length>16?"Car Dead":"Woohoo!"
