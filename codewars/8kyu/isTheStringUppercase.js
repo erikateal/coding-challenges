3acc03aded08bc35682a21e1d612a46bb101db00
Erika Teal <erikamw2070@yahoo.com>
2022-09-19 19:54:00 -0500

    9/19 coding challenge

---
+++ codewars/8kyu/isTheStringUppercase.js
@@ -0,0 +1,25 @@
+//OBJECTIVE
+// Is the string uppercase?
+
+//LINK
+// https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+
+// INSTRUCTIONS
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+// MY SOLUTION
+String.prototype.isUpperCase = function () {
+  return this == this.toUpperCase() ? true : false;
+};
+
+// BEST PRACTICE
+// String.prototype.isUpperCase = function() {return this==this.toUpperCase()}
