e2de1b394c5bd3d7e5df6c8fe31accd4c74f6250
Erika Teal <erikamw2070@yahoo.com>
2022-10-22 14:41:22 -0500

    10/22 coding challenge

---
+++ codewars/8kyu/removeFirstAndLastCharacter.php
@@ -0,0 +1,14 @@
+<!-- OBJECTIVE -->
+<!-- Remove First and Last Character -->
+
+<!-- LINK -->
+<!-- https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0 -->
+
+<!-- INSTRUCTIONS -->
+<!-- It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters. -->
+
+<!-- MY SOLUTION -->
+function remove_char(string $s): string {
+  return substr($s,1,-1);
+}
+
