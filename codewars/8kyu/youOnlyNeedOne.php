04071c2497145b4f3bf12beef8e9236c8cfc39d0
Erika Teal <erikamw2070@yahoo.com>
2022-10-27 15:11:20 -0500

    10/27 coding challenge

---
+++ codewars/8kyu/youOnlyNeedOne.php
@@ -0,0 +1,19 @@
+<?php
+
+// OBJECTIVE
+// You only need one - Beginner
+
+// LINK
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b
+
+// INSTRUCTIONS
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// MY SOLUTION
+function solution($a, $x) {
+  return in_array($x, $a);
+}
